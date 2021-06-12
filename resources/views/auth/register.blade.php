471aeca6270d9e28819582ab70b4e3ddf633fa22
wilmar.rincon <wilmar.rincon@linktic.com>
2021-06-12 16:25:00 -0500

    Agregar nombres a los botones para las pruebas con selenium

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,7 +66,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" name="register" class="btn btn-primary">
                                     {{ __('Regístrame') }}
                                 </button>
                             </div>
